8b82f5da5f6ff0c845c518c73a0eccc2bb49f0e0
ramadan987 <maullanarmadhannnnn@gmail.com>
2025-04-24 16:41:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, nobis velit, natus reprehenderit vel repellat delectus sapiente, veniam quam laboriosam voluptas voluptatum? Fuga dolorum nemo temporibus deleniti nisi corrupti voluptate?</p>
+    <ul>
+        <li>1234567</li>
+        <li>123456787654322345675432123456</li>
+    </ul>
 </body>
 </html>
